7a197d8d7f18743a4cb1e46c589a23d75ada63a8
m.reine <bonjourman78@gmail.com>
2022-05-23 16:59:50 +0200

    final

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/ProductRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+import path from 'path';
+import uploadRouter from './routes/uploadRoutes.js';
 
 dotenv.config();
 
@@ -24,6 +26,7 @@ app.get('/api/keys/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
   });
 
+app.use('/api/upload', uploadRouter);
 app.use('/api/seed',seedRouter);
 app.use('/api/products',productRouter);
 app.use('/api/users',userRouter);
@@ -32,6 +35,12 @@ app.use('/api/orders', orderRouter);
 app.use((err,req,res,next) =>{
     res.status(500).send({message: err.message});
 });
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 app.get('/api/products',(req,res) =>{
     res.send(data.products);
